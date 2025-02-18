d249c44c2b2270ebf5c24eb39391fd4e17e4b7ad
CristopherRms <123616833+CristopherRms@users.noreply.github.com>
2025-02-17 21:10:40 -0600

    .
    .

---
+++ src/views/gestionRiesgos.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+function GestionRiesgos() {
+  return (
+    <Box sx={{ p: 4 }}>
+      <h1 style={{
+          textAlign: "center",
+          marginBottom: "32px",
+          fontFamily: "'Roboto', sans-serif",
+          color: "#004A98",
+        }}
+      >
+        Gestión de Riesgos
+      </h1>
+    </Box>
+  );
+}
+
+export default GestionRiesgos;
