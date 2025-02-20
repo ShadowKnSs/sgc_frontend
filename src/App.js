17844a03795632ccb9a50bdf84735ecedf65124d
TheDarkfoxs <losacks123@gmail.com>
2025-02-19 19:30:38 -0600

    (ID Act. 1.8) Se disenó la interfaz de esctructura del proceso (falta agregar header)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NewProcess from './views/newProcess';
 import Welcome from './views/welcome';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import ProcessList from './views/processList';
-
+import ProcessStructure from './views/procesStructure';
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
         <Route path='/' element={<Welcome />}></Route>
         <Route path='/newP' element={<NewProcess />}/>
         <Route path="/procesos" element={<ProcessList />} />
+        <Route path="/pros" element={<ProcessStructure />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/views/procesStructure.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Box } from "@mui/material";
+import MenuCard from "../components/menuCard";
+import BookIcon from "@mui/icons-material/Book";
+import WarningIcon from "@mui/icons-material/Warning";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import DescriptionIcon from "@mui/icons-material/Description";
+import LinkIcon from "@mui/icons-material/Link";
+import BarChartIcon from "@mui/icons-material/BarChart";
+
+const ProcessStructure = () => {
+  const menuItems = [
+    { icon: <BookIcon />, title: "Manual Operativo" },
+    { icon: <WarningIcon />, title: "Gestión de Riesgo" },
+    { icon: <InsertDriveFileIcon />, title: "Análisis de Datos" },
+    { icon: <TrendingUpIcon />, title: "Acciones de Mejora" },
+    { icon: <DescriptionIcon />, title: "Generar informe de auditoria" },
+    { icon: <LinkIcon />, title: "Seguimiento" },
+    { icon: <BarChartIcon />, title: "Indicadores" },
+  ];
+
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        gap: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        padding: 2,
+        maxWidth: "900px",
+        margin: "auto",
+      }}
+    >
+      {menuItems.slice(0, 4).map((item, index) => (
+        <MenuCard key={index} icon={item.icon} title={item.title} sx={{ textAlign: "center" }} />
+      ))}
+      <Box
+        sx={{
+          gridColumn: "span 4",
+          display: "flex",
+          justifyContent: "center",
+          gap: 10,
+          marginTop: -30,
+        }}
+      >
+        {menuItems.slice(4).map((item, index) => (
+          <MenuCard key={index + 4} icon={item.icon} title={item.title} sx={{ textAlign: "center" }} />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default ProcessStructure;
