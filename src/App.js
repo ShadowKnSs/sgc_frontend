b18c9e3785de680c3496d6f12549ec9081e89549
Pgrg <palmirag211@gmail.com>
2025-02-22 18:12:41 -0600

    Implementacion de las vistas para la gestion de usuarios, asi como de la vista para visualizar las entidades

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NewProcess from './views/newProcess';
 import Welcome from './views/welcome';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import ProcessList from './views/processList';
+import Entity from './views/entity';
+import UserManagement from './views/usersList';
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Route path='/' element={<Welcome />}></Route>
         <Route path='/newP' element={<NewProcess />}/>
         <Route path="/procesos" element={<ProcessList />} />
+        <Route path="/entidad" element={<Entity />} />
+        <Route path="/users" element={<UserManagement />}/>
       </Routes>
     </BrowserRouter>
   )
