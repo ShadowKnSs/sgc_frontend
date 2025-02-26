ebf35bb02ac521d5c840a0043e5e5a5741e6930e
Pgrg <palmirag211@gmail.com>
2025-02-26 10:35:01 -0600

    Actualizacion del componente menu card, asi como de las vistas de inicio y de entidades.

--- src/views/entity.jsx
+++ src/views/entity.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Box } from "@mui/material";
-import MenuCard from "../components/MenuCard";
 import BookIcon from "@mui/icons-material/Book";
 import LanguageIcon from "@mui/icons-material/Language";
 import PeopleIcon from "@mui/icons-material/People";
@@ -9,29 +8,36 @@ import DescriptionIcon from "@mui/icons-material/Description";
 import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 import BarChartIcon from "@mui/icons-material/BarChart";
+import MenuCard from "../components/menuCard";
 
 const Entity = () => {
   const menuItems = [
     { icon: <BookIcon />, title: "Facultad de Enfermería " },
-    { icon: <LanguageIcon />, title: "Facultad de Economía " },
+    { icon: <LanguageIcon />, title: "Unidad Academica Multidisciplinaria Region Altiplano" },
     { icon: <PeopleIcon />, title: "Facultad de Ingeniería " },
     { icon: <AccountTreeIcon />, title: "Facultad de Ciencias Sociales y Humanidades" },
     { icon: <DescriptionIcon />, title: "Facultad de Agronomía y Veterinaria " },
-    { icon: <VerifiedUserIcon />, title: "Facultad de Derecho " },
-    { icon: <SupervisorAccountIcon />, title: "Facultad de Ciencias de la Comunicación " },
-    { icon: <BarChartIcon />, title: "Facultad de Ciencias Químicas " },
+    { icon: <VerifiedUserIcon />, title: "Facultad de Estudios Profesionales Zona Huasteca" },
+    { icon: <SupervisorAccountIcon />, title: "Facultad de Ciencias de la Comunicación" },
+    { icon: <BarChartIcon />, title: "Coordinación Académica Región Altiplano Oeste" },
+    { icon: <DescriptionIcon />, title: "Facultad de Agronomía y Veterinaria " },
+    { icon: <VerifiedUserIcon />, title: "Facultad de Estudios Profesionales Zona Huasteca" },
+    { icon: <SupervisorAccountIcon />, title: "Facultad de Ciencias de la Comunicación" },
+    { icon: <BarChartIcon />, title: "Coordinación Académica Región Altiplano Oeste" },
   ];
 
   return (
     <Box
       sx={{
         display: "grid",
-        gridTemplateColumns: "repeat(4, 1fr)",
-        gap: 3,
+        gridTemplateColumns: "repeat(5, auto)", 
+        gap: 8, 
+        placeItems: "center", 
         justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        padding: 2,
+        minHeight: "100vh", 
+        paddingTop: "80px", 
+        paddingBottom: "40px", 
+        width: "100%",
       }}
     >
       {menuItems.map((item, index) => (
